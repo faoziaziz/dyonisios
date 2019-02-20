22cf592dfa9fb5474640ea8be2d353471a4884ca
Aziz Amerul Faozi <admin@labseni.com>
2019-02-20 21:46:25 +0700

    asek

--- api/routes/user.route.js
+++ api/routes/user.route.js
@@ -1,6 +1,28 @@
 module.exports = (app) => {
-    const page = require('../kontroller/page/fdtdCtrl.js');
-    // Route ke fdtd
-    app.get('/fdtd', page.fdtd);
+    const user = require('../kontroller/userCtrl.js');
 
+    // Bikin buku baru
+    app.post('/users', user.insertUser);
+    // tampil semua bukunya
+    app.get('/users', user.tampilDetail);
+    // cari 1 buku berdasar idnya
+    app.get('/asek', function(req, res){
+      res.json({
+        "anjir":"banget"
+      });
+    });
+    // laman utama
+    app.get('/', function(req, res){
+      res.render('index', { title: 'Labseni' });
+    });
+
+    app.get('/users/:userID', user.cariSatu);
+    app.get('/daftar', function(req, res){
+      res.render('daftar', {
+        judul: "Daftarin Bang"
+      });
+    });
+
+    app.get('/kalista', user.kalistaTest);
 }
+
