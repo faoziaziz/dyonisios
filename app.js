dfe4a5b9bfbfc7732b22abdb84b380d919303b74
Dean Iqbal Divanda <dean.iqbal@prasimax.com>
2019-02-22 10:12:58 +0700

    asek

--- app.js
+++ app.js
@@ -5,8 +5,7 @@ var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
+
 var mongoose = require('mongoose');
 var app = express();
 
